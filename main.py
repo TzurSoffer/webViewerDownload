f595c4d8c1c8662254154b0205e4c1b1ec142cfd
Tzur <103438808+RandomTheFirst@users.noreply.github.com>
2023-07-01 19:40:06 -0700

    bugfix and refactor for webViewer

--- main.py
+++ main.py
@@ -1,138 +1,193 @@
-from flask import Flask, render_template, Response, send_file
-import cv2
-import av
-import os
-import base64
-import threading
-import time
-import io
+import time, os
 from datetime import datetime
 from collections import deque
+import io
+
+import cv2
+import av
+from flask import render_template, Response
 
 from webVideoViewer import webViewer
 
-class Downloader:
-    def __init__(self, downloadFPS, downloadResWidth, downloadPreviusTime=24):
-        self.buffer = deque(maxlen=int(downloadPreviusTime*60*60*downloadFPS))
+class Downloader():
+    """Class responsible for downloading and creating videos."""
+    def __init__(self, downloadEvery, downloadResWidth, downloadPreviousTime=24):
+        """
+        Initialize the Downloader object.
+
+        Args:
+            downloadEvery (float): The time interval between frame downloads in seconds.
+            downloadResWidth (int): The width of the downloaded frames.
+            downloadPreviousTime (int, optional): The maximum amount of time to save frames for downloading in hours.
+        """
+        self.buffer = deque(maxlen=int(downloadPreviousTime*60*60*downloadEvery)) #< the max amount of frames in the video 
         self.first = True
-        self.downloadPreviusTime = downloadPreviusTime
-        self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        self.timeBetweenReads = 1/downloadFPS
-        self.downloadResWidth = downloadResWidth
-        self.lastFrameTimestamp = time.time()
+        self.downloadPreviousTime = downloadPreviousTime                          #< the max amount of time to save in bugger for downloading                         
+        self.timeBetweenReads = 1/downloadEvery                                   #< the amount of time to wait in seconds between reading frames
+        self.downloadResWidth = downloadResWidth                                  #< the resolution to set the frame for downloading
+        self.lastFrameTimestamp = time.time()                                     #< timestamp of the last frame added to the buffer
 
-    def addFrame(self, frame):
-        shape = (frame.shape[1], frame.shape[0])
-        if self.first == True:
+    def addFrame(self, frame) -> None:
+        """
+        Add a frame to the buffer.
+
+        Args:
+            frame (numpy.ndarray): The frame to add.
+        """
+        shape = (frame.shape[1], frame.shape[0])                                  #< the shape of the image
+        if self.first == True:                                                    #< is it the first frame added
             self.first = False
-            scaleFactor = self.downloadResWidth/shape[0]
-            self.newWidth = int(self.downloadResWidth)
-            self.newHeight = int(shape[1]*scaleFactor)
+            scaleFactor = self.downloadResWidth/shape[0]                          #< get the scale factor for resizing the image
+            self.newWidth = int(self.downloadResWidth)                            #< the new width of the image
+            self.newHeight = int(shape[1]*scaleFactor)                            #< the new height of the image
 
+        if time.time()-self.lastFrameTimestamp >= self.timeBetweenReads:          #< is timeBetweenReads past since last frame was read
+            if len(frame.shape) == 3:                                             #< Check if the frame is in color (has 3 channels)
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)                   #< Convert the frame to grayscale
 
+            scaled = cv2.resize(frame, (self.newWidth, self.newHeight))           #< Resize the frame to the specified resolution
 
-        if time.time()-self.lastFrameTimestamp >= self.timeBetweenReads:
-            if len(frame.shape) == 3:
-                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-            scaled = cv2.resize(frame, (self.newWidth, self.newHeight))
-
-            self.buffer.append(scaled)
-            self.lastFrameTimestamp = time.time()
+            self.buffer.append(scaled)                                            #< Add the resized frame to the buffer
+            self.lastFrameTimestamp = time.time()                                 #< Update the timestamp of the last frame added
 
     def download(self):
-        output = io.BytesIO()
-        # Create a container with a video stream
-        container = av.open(output, 'w', format='mp4')
+        """
+        Create a video file from the frames in the buffer.
 
-        # Add a video stream to the container
-        stream = container.add_stream('h264', rate=20)
-        stream.width = self.downloadResWidth
-        stream.height = self.buffer[0].shape[0]
+        Returns:
+            bytes: The video file data.
+        """
+        output = io.BytesIO()                                                     #< Create a BytesIO object to store the video file data
+        container = av.open(output, 'w', format='mp4')                            #< Create a container with a video stream
 
-        # Create a copy of the buffer before iterating over it
-        buffer_copy = self.buffer.copy()
+        stream = container.add_stream('h264', rate=20)                            #< Add a video stream to the container
+        stream.width = self.downloadResWidth                                      #< Set the width of the video stream
+        stream.height = self.buffer[0].shape[0]                                   #< Set the height of the video stream
 
-        for frame in self.buffer:
+        buffer_copy = self.buffer.copy()                                          #< Create a copy of the buffer so i dont iterate over it while adding a frame to it
+
+        for frame in buffer_copy:
             # Convert the frame to an AVFrame object
-            av_frame = av.VideoFrame.from_ndarray(frame, format='gray')
+            av_frame = av.VideoFrame.from_ndarray(frame, format='gray')           #< Convert the frame to an AVFrame object
 
-            # Rescale the frame if necessary
-##            if frame.width != stream.width or frame.height != stream.height:
-##                av_frame = av_frame.reformat(stream.width, stream.height)
-
-            # Encode the frame and write it to the video stream
-            packet = stream.encode(av_frame)
+            packet = stream.encode(av_frame)                                      #< Encode the frame and write it to the video stream
             container.mux(packet)
 
-        # Flush any remaining frames
-        packet = stream.encode()
-        container.mux(packet)
+        packet = stream.encode()                                                  #< Flush any remaining frames
+        container.mux(packet) 
         container.close()
 
         output.seek(0)
         return(output.getvalue())
 
-    def _getTimestamp(self):
-        now = datetime.now()
-        formattedTime = now.strftime('%Y-%m-%d___%H-%M-%S')
-        return (formattedTime)
-
 class VideoStream(webViewer.VideoStream):
+    """Class representing a video streaming application."""
     def __init__(self,
                  homePageTemplate="./webVideoViewer/templates/home.html",
                  subpageTemplate="./templates/subpage.html",
                  port=80,
-                 downloadFPS=0.5,
+                 downloadEvery=0.5,
                  downloadResWidth=200,
-                 downloadPreviusTime=24 #< in hours
+                 downloadPreviousTime=24 #< in hours
                  ):
+        """
+        Initialize the VideoStream object.
+
+        Args:
+            homePageTemplate (str, optional): Path to the home page template file.
+            subpageTemplate (str, optional): Path to the subpage template file.
+            port (int, optional): The port number for running the Flask application.
+            downloadEvery (float, optional): The time interval between frame downloads in seconds.
+            downloadResWidth (int, optional): The width of the downloaded frames.
+            downloadPreviousTime (int, optional): The maximum amount of time to save frames for downloading in hours.
+        """
 
         super().__init__(homePageTemplate=homePageTemplate,
                          subpageTemplate=subpageTemplate,
                          port=port, currentFolder = os.path.dirname(os.path.abspath(__file__)))
 
-        self.downloadPreviusTime = downloadPreviusTime
-        self.downloadFPS = downloadFPS
-        self.downloadResWidth = downloadResWidth
-        self.downloaders = {}
+        self.downloadPreviousTime = downloadPreviousTime                           #< The maximum amount of time to save frames for downloading (in hours)
+        self.downloadEvery = downloadEvery                                         #< The time interval between frame downloads in seconds
+        self.downloadResWidth = downloadResWidth                                   #< The width of the downloaded frames
+        self.downloaders = {}                                                      #< Dictionary to store Downloader objects for each subpage
 
-        # Buffer to hold frames for 1 hour (60 minutes * 60 seconds)
+    def _getTimestamp(self) -> str:
+        """
+        Get the current timestamp as a formatted string.
 
+        Returns:
+            str: The formatted timestamp.
+        """
+        now = datetime.now()
+        formattedTime = now.strftime('%Y-%m-%d___%H-%M-%S')
+        return(formattedTime)
 
     def setupRoutes(self):
+        """Set up the routes for the Flask application."""
         super().setupRoutes()
-        self.app.route('/<name>/download')(self.download)
+        self.app.route('/<name>/download')(self.download)                          #< Define the route for downloading the recorded video
 
     def renderTemplate(self, name):
-        return render_template(self.subpageTemplate, video_feed=f"/{name}/videoFeed", download_url=f"/{name}/download", donload_length=str(self.downloadPreviusTime))
+        """
+        Render the template for a given name.
+
+        Args:
+            name (str): The name of the template.
+
+        Returns:
+            str: The rendered template.
+        """
+        return(render_template(self.subpageTemplate,
+                               video_feed=f"/{name}/videoFeed",
+                               download_url=f"/{name}/download",
+                               download_length=str(self.downloadPreviousTime))
+               ) #< Render the subpage template
 
     def download(self, name):
+        """
+        Download the recorded video for a given name.
+
+        Args:
+            name (str): The name(subpage) associated with the video.
+
+        Returns:
+            Response: The response object containing the video file data.
+        """
         try:
             downloader = self.downloaders[name]
-            video_data = downloader.download()
+            video_data = downloader.download()                                     #< Call the download method of the corresponding Downloader object
+            fileName = f"last_{self.downloadPreviousTime}_hours_{self._getTimestamp()}.mp4"
 
-            return Response(
+            return(Response(
                 video_data,
                 mimetype='video/mp4',
-                headers={'Content-Disposition': f'attachment; filename=last_{self.downloadPreviusTime}_hours.mp4'}
-            )
+                headers={'Content-Disposition': f'attachment; filename={fileName}'}
+            ))
         except Exception as e:
             print(e)
-            return 'Error'
+            return('Error')
 
     def imshow(self, name, image):
-        super().imshow(name, image)
+        """
+        Display an image on the subpage with the given name.
+
+        Args:
+            name (str): The name of the subpage.
+            image (numpy.ndarray): The image to display.
+        """
+        super().imshow(name, image)                                                #< Call the imshow method of the parent class
         if name not in self.downloaders.keys():
-            self.downloaders[name] = Downloader(self.downloadFPS, self.downloadResWidth, downloadPreviusTime=self.downloadPreviusTime)
-        self.downloaders[name].addFrame(image)
+            self.downloaders[name] = Downloader(self.downloadEvery,
+                                                self.downloadResWidth,
+                                                downloadPreviousTime=self.downloadPreviousTime
+                                                )                                  #< Create a Downloader object for the subpage if it doesn't exist
+        self.downloaders[name].addFrame(image)                                     #< Add the frame to the corresponding Downloader object
 
 if __name__ == '__main__':
-    app = VideoStream(downloadPreviusTime=24)
     cap = cv2.VideoCapture(0)
-    
+    app = VideoStream(downloadPreviousTime=24)
     app.run()
-    
+
     # Continuously capture frames from the webcam and update subpages
     while True:
         _, frame = cap.read()
--- webVideoViewer/webViewer.py
+++ webVideoViewer/webViewer.py
@@ -4,80 +4,95 @@ import cv2
 import os
 import base64
 
-class SubpageManager:
-    def __init__(self):
-        self.frames = {}
-    
-    def imshow(self, name, frame):
-        self.frames[name] = frame
-    
-    def getFrame(self, name):
-        return self.frames.get(name, None)
+class SubpageManager(dict):
+    def __getitem__(self, __key):
+        return(self.get(__key, None))
 
-class VideoStream:
-    def __init__(self, homePageTemplate="templates/home.html", subpageTemplate="templates/subpage.html", port=80, currentFolder = os.path.dirname(os.path.abspath(__file__))):
+class VideoStream():
+    """
+    """
+    def __init__(self, homePageTemplate="templates/home.html", subpageTemplate="templates/subpage.html", port=80, currentFolder=os.path.dirname(os.path.abspath(__file__))):
+        """
+        Initializes a VideoStream object.
+
+        Args:
+            homePageTemplate (str): Path to the home page template file.
+            subpageTemplate (str): Path to the subpage template file.
+            port (int): Port number for the Flask application.
+            currentFolder (str): Path to the current folder containing the script.
+        """
         self.app = Flask(__name__, template_folder=currentFolder)
-
         self.manager = SubpageManager()
-
         self.port = port
 
         self.setupRoutes()
         self.homePageTemplate = homePageTemplate
         self.subpageTemplate = subpageTemplate
-        self.subpages = []
-        self.homePage = "<h1>Subpages for Names</h1><ul>{}</ul>"
 
-    def setupRoutes(self):
-        self.app.route('/')(self.index)
-        self.app.route('/<name>')(self.renderTemplate)
-        self.app.route('/<name>/videoFeed')(self.subpage)
+    def setupRoutes(self) -> None:
+        """Sets up the routes for the Flask application"""
+        self.app.route('/')(self.index)  #< Define the route for the home page
+        self.app.route('/<name>')(self.renderTemplate)  #< Define the route for the subpages
+        self.app.route('/<name>/videoFeed')(self.subpage)  #< Define the route for the video feed of each subpage
 
-    def addSubpage(self, name):
-        self.subpages.append(name)
-    
-    def index(self):
-        return(render_template(self.homePageTemplate, subpages=self.subpages))
+    def index(self) -> str:
+        """Returns the rendered HTML content for the home page"""
+        return render_template(self.homePageTemplate, subpages=list(self.manager.keys()))  #< Render the home page template
 
-    def renderTemplate(self, name):
-        return(render_template(self.subpageTemplate, video_feed=f"/{name}/videoFeed"))
-    
+    def renderTemplate(self, subpage) -> str:
+        """Returns the rendered subpage(str)"""
+        return render_template(self.subpageTemplate, video_feed=f"/{subpage}/videoFeed")  #< Render the subpage template
+
     def subpage(self, name):
-        def generate_frames():
+        """
+        Generates imgs for the subpage.
+
+        Args:
+            name (str): Name of the subpage.
+
+        Returns:
+            Response: Response object containing the generated imgs.
+        """
+        def generate_imgs():
             while True:
-                frame = self.manager.getFrame(name)
-                if frame is None:
-                    yield b'--frame\r\n'
-                    yield b'Content-Type: text/html\r\n\r\n'
-                    yield b'<h1>No subpage found for {name}</h1>\r\n\r\n'
-                else:
-                    yield b'--frame\r\n'
+                img = self.manager[name]
+                if img:
+                    yield b'--img\r\n'
                     yield b'Content-Type: image/jpeg\r\n\r\n'
-                    yield base64.b64decode(frame)
+                    yield base64.b64decode(img)  #< Return the img in the response
+                else:
+                    yield b'--img\r\n'
+                    yield b'Content-Type: text/html\r\n\r\n'
+                    yield b'<h1>No subpage found for {name}</h1>\r\n\r\n'  #< Return a message if no img is found for the subpage
 
-        return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-    
-    def imshow(self, name, frame):
-        if self.manager.getFrame(name) is None:   # subpage doesn't exist
-            self.addSubpage(name)
+        return Response(generate_imgs(), mimetype='multipart/x-mixed-replace; boundary=img')  #< Return the response object
 
-        _, buffer = cv2.imencode('.jpg', frame)
-        frame = base64.b64encode(buffer).decode('utf-8')
-        self.manager.imshow(name, frame)
-    
-    def run(self):
-        threading.Thread(target=self.app.run, kwargs={"host": "0.0.0.0", "port": self.port}).start()
+    def imshow(self, name, img) -> None:
+        """
+        Displays an image on a subpage.
+
+        Args:
+            name (str): Name of the subpage.
+            img: Image to be displayed.
+        """
+        _, buffer = cv2.imencode('.jpg', img)          #< Encode the img as a JPEG image
+        img = base64.b64encode(buffer).decode('utf-8') #< Encode the image data as base64 string
+        self.manager[name] = img                       #< Store the img in the SubpageManager
+
+    def run(self) -> None:
+        """Runs the Flask application in a separate thread"""
+        threading.Thread(target=self.app.run, kwargs={"host": "0.0.0.0", "port": self.port}).start()  #< Run the Flask application in a separate thread
 
 
 if __name__ == '__main__':
-    app = VideoStream()
-    cap = cv2.VideoCapture(0)
-    
-    app.run()
-    
-    # Continuously capture frames from the webcam and update subpages
+    cap = cv2.VideoCapture(0)  #< Open the webcam capture device
+    app = VideoStream()        #< Create a VideoStream object
+    app.run()                  #< Run the Flask application
+
+    # Continuously capture imgs from the webcam and update subpages
     while True:
-        _, frame = cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        app.imshow("frame", frame)
-        app.imshow("gray", gray)
+        _, frame = cap.read()                          #< Read a frame from the webcam
+        app.imshow("frame", frame)                     #< Display the frame on the "frame" subpage
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #< Convert the frame to grayscale
+        app.imshow("gray", gray)                       #< Display the grayscale frame on the "gray" subpage
